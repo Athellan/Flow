77a9a83162a35ac6092ef8a453890567fd1035ea
Athellan <jeanbaptiste.cambillard@gmail.com>
2022-10-20 13:11:36 +0200

    Initial Commit

---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by Apprenant 63 on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
