5ae22a7181165df7221ca34d8623e49897338875
Inès <ines.haegelin@gmail.com>
2022-10-25 09:25:12 +0200

    ajout profil category preference

---
+++ Flow/components/profileCategoryPreference.swift
@@ -0,0 +1,44 @@
+//
+//  profileCategoryPreference.swift
+//  Flow
+//
+//  Created by Apprenant97 on 24/10/2022.
+//
+
+import SwiftUI
+struct Picto: Identifiable {
+    let image: String
+    var id: String { image }
+    
+}
+
+let movieCategories: [Picto] = [
+    Picto(image: "history"),
+    Picto(image: "horror"),
+    Picto(image: "mystery"),
+    Picto(image: "sci-fi")
+]
+
+struct profileCategoryPreference: View {
+    var movieCategory : Picto
+
+//    let movieCategories = ["history", "horror", "sci-fi", "mystery"]
+    
+    var body: some View {
+        ZStack {
+            Image("\(movieCategory.image)")
+                .resizable()
+                .frame(width: 72, height: 72)
+        }
+        .frame(width: 100, height: 100)
+        .background(Color("highlightColor"))
+        .cornerRadius(10)
+        }
+    }
+
+
+struct profileCategoryPreference_Previews: PreviewProvider {
+    static var previews: some View {
+        profileCategoryPreference(movieCategory: movieCategories[0])
+    }
+}
